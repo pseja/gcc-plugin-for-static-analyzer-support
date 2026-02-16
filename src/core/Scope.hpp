c0ff6fe9cfe2f23dd2089829a3b11be16c8d9650
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-16 19:55:59 +0100

    Scope enum class

---
+++ src/core/Scope.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace CodeListener
+{
+
+namespace Core
+{
+
+enum class Scope
+{
+    GLOBAL,
+    STATIC,
+    FUNCTION,
+};
+
+} // namespace Core
+
+} // namespace CodeListener
