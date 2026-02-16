e7664630ba5ca8b0b66be84c567df8b258889c1e
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-16 19:52:24 +0100

    NodeId enum class

---
+++ src/core/NodeId.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdint>
+
+namespace CodeListener
+{
+
+namespace Core
+{
+
+enum class NodeId : uint32_t
+{
+    INVALID = 0,
+};
+
+} // namespace Core
+
+} // namespace CodeListener
