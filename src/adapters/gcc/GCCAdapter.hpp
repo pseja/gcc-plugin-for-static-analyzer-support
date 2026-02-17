b432e1ebdfbe0b23a4cf33e8aca6198994bf88ab
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-17 18:05:31 +0100

    GCCAdapter class

---
+++ src/adapters/gcc/GCCAdapter.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <gcc-plugin.h>
+
+#include <unordered_map> // std::unordered_map
+
+#include "CodeModel.hpp"
+#include "NodeId.hpp"
+#include "SourceLocation.hpp"
+
+namespace CodeListener
+{
+
+namespace Core
+{
+
+class GCCAdapter
+{
+  public:
+    GCCAdapter(CodeModel &model);
+
+    void processFunction(function *fun);
+
+  private:
+    CodeModel &model;
+
+    std::string current_function_name;
+    std::unordered_map<tree, NodeId> variable_cache;
+    std::unordered_map<tree, NodeId> type_cache;
+
+    NodeId getOrCreateType(tree type_tree);
+    NodeId getOrCreateVariable(tree var_tree);
+    Operand parseOperand(tree operand_tree);
+
+    SourceLocation getSourceLocation(location_t location);
+
+    TypeKind mapTypeTreeToTypeKind(tree &type_tree);
+};
+
+} // namespace Core
+
+} // namespace CodeListener
