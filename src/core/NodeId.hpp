584cb244551a11042b22417d9ffd5dc5fb30a2aa
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-17 18:01:29 +0100

    NodeId toString

--- src/core/NodeId.hpp
+++ src/core/NodeId.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 namespace CodeListener
 {
@@ -12,6 +13,15 @@ enum class NodeId : uint32_t
 {
     INVALID = 0,
 };
+
+inline std::string toString(NodeId id)
+{
+    if (id == NodeId::INVALID)
+    {
+        return "NodeId(INVALID)";
+    }
+    return "NodeId(" + std::to_string(static_cast<uint32_t>(id)) + ")";
+}
 
 } // namespace Core
 
