2e145c20ac72e530dd353cd4c65733032cefe399
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-20 20:23:07 +0100

    add basic basic block processing

--- src/adapters/gcc/GCCAdapter.cpp
+++ src/adapters/gcc/GCCAdapter.cpp
@@ -1,12 +1,21 @@
+// there is an enum conversion warning in a GCC header
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-enum-enum-conversion"
+
 #include <gcc-plugin.h>
-#include <function.h> // function
-#include <tree.h> // DECL_NAME
+#include <tree.h>            // DECL_NAME
+#include <function.h>        // function
+#include <gimple.h>          // gimple_location
+#include <gimple-iterator.h> // gimple_stmt_iterator, gsi_start_bb, gsi_end_p, gsi_next, gsi_stmt
+
+#pragma GCC diagnostic pop
 
 #include "GCCAdapter.hpp"
 #include "NodeId.hpp"
 #include "PluginContext.hpp"
 #include "SourceLocation.hpp"
 #include "Type.hpp"
+#include "Operand.hpp"
 
 namespace CodeListener
 {
@@ -56,6 +65,7 @@ SourceLocation GCCAdapter::getSourceLocation(location_t location)
 {
     if (location == UNKNOWN_LOCATION)
     {
+        // FIXME: before return statement, there is a label to jump to unknown location for some reason
         CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
             DiagnosticLevel::Warning, "Encountered an unknown source location");
 
@@ -203,6 +213,54 @@ NodeId GCCAdapter::getOrCreateVariable(tree variable_tree)
     return variable.id;
 }
 
+Operand GCCAdapter::parseOperand(tree operand_tree)
+{
+    if (!operand_tree)
+    {
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Warning, "Encountered an operand without a tree representation");
+        return ConstantOperand{NodeId::INVALID, "<invalid>"};
+    }
+
+    CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(DiagnosticLevel::Debug,
+                                                                                          "Processing operand tree...");
+
+    if (DECL_P(operand_tree) || (TREE_CODE(operand_tree) == SSA_NAME))
+    {
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Debug, "Operand is a variable, processing as variable operand...");
+
+        // TODO: handle SSA_NAMEs properly, they can represent both variables and temporary values
+        VariableOperand variable_operand;
+        variable_operand.variable_id = getOrCreateVariable(operand_tree);
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Debug, "Parsed variable operand with ID: " + toString(variable_operand.variable_id));
+
+        return variable_operand;
+    }
+    else if (CONSTANT_CLASS_P(operand_tree))
+    {
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Debug, "Operand is a constant, processing as constant operand...");
+
+        // FIXME: very rough way to represent constants, do proper handling for different constant types
+        ConstantOperand constant_operand;
+        // FIXME: replace with proper ID generation
+        constant_operand.id = static_cast<NodeId>(reinterpret_cast<uintptr_t>(operand_tree));
+        // TODO: extract actual constant value
+        constant_operand.value = "<constant>";
+
+        return constant_operand;
+    }
+    else
+    {
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Warning, "Encountered an unhandled operand type in gimple statement");
+    }
+
+    return ConstantOperand(NodeId::INVALID, "<unhandled>");
+}
+
 void GCCAdapter::processFunction(function *fun)
 {
     if (!fun || DECL_NAME(fun->decl) == nullptr)
@@ -214,11 +272,12 @@ void GCCAdapter::processFunction(function *fun)
 
     CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
         DiagnosticLevel::Debug, std::string("Adapter processing function: ") + function_name(fun));
-    
+
     // save the current function name for source location tracking
     // TODO: maybe bring the function name through function arguments instead of Adapter attribute
     current_function_name = function_name(fun);
 
+    // process function declaration
     tree function_declaration = fun->decl;
     Function function_node;
     // FIXME: replace with proper ID generation
@@ -244,7 +303,84 @@ void GCCAdapter::processFunction(function *fun)
             DiagnosticLevel::Debug, "Processed parameter with ID: " + toString(function_node.parameter_ids.back()));
     }
 
-    // TODO: process local variables
+    // process local variables
+    basic_block bb;
+    FOR_EACH_BB_FN(bb, fun)
+    {
+        Block block_node;
+        // FIXME: replace with proper ID generation
+        block_node.id = static_cast<NodeId>(reinterpret_cast<uintptr_t>(bb));
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Debug, "Block node ID: " + toString(block_node.id));
+        block_node.parent_function_id = function_node.id;
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Debug, "Block node parent function ID: " + toString(block_node.parent_function_id));
+        block_node.name = "bb_" + std::to_string(bb->index);
+        CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+            DiagnosticLevel::Debug, "Block node name: " + block_node.name);
+
+        // process instructions in the block
+        for (gimple_stmt_iterator gsi = gsi_start_bb(bb); !gsi_end_p(gsi); gsi_next(&gsi))
+        {
+            gimple *stmt = gsi_stmt(gsi);
+
+            Instruction instruction_node;
+            instruction_node.id = static_cast<NodeId>(reinterpret_cast<uintptr_t>(stmt));
+            CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+                DiagnosticLevel::Debug, "Instruction node ID: " + toString(instruction_node.id));
+            instruction_node.parent_block_id = block_node.id;
+            CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+                DiagnosticLevel::Debug,
+                "Instruction node parent block ID: " + toString(instruction_node.parent_block_id));
+            instruction_node.source_location = getSourceLocation(gimple_location(stmt));
+            CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+                DiagnosticLevel::Debug,
+                "Instruction node source location: " + toString(instruction_node.source_location));
+            // determine opcode
+            enum gimple_code gcode = gimple_code(stmt);
+            instruction_node.opcode = gimple_code_name[gcode];
+            CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+                DiagnosticLevel::Debug, "Processed instruction with opcode: " + instruction_node.opcode);
+
+            // extract operands based on statement type
+            if (is_gimple_assign(stmt))
+            {
+                tree lhs = gimple_assign_lhs(stmt);
+                tree rhs1 = gimple_assign_rhs1(stmt);
+                instruction_node.operands.push_back(parseOperand(lhs));
+                instruction_node.operands.push_back(parseOperand(rhs1));
+
+                if (gimple_num_ops(stmt) > 2)
+                {
+                    tree rhs2 = gimple_assign_rhs2(stmt);
+                    if (rhs2)
+                    {
+                        instruction_node.operands.push_back(parseOperand(rhs2));
+                    }
+                }
+            }
+            // TODO: handle more statement types
+            else
+            {
+                CompilerAbstractionLayer::PluginContext::getInstance().getDiagnosticReporter().report(
+                    DiagnosticLevel::Warning,
+                    "Encountered an unhandled gimple statement type: " + instruction_node.opcode);
+            }
+
+            // FIXME: uncomment after implementing the method
+            // model.addInstruction(instruction_node);
+            block_node.instruction_ids.push_back(instruction_node.id);
+        }
+
+        // FIXME: uncomment after implementing the method
+        // model.addBlock(block_node);
+        function_node.block_ids.push_back(block_node.id);
+    }
+
+    // TODO: process CFG edges between blocks
+
+    // FIXME: uncomment after implementing the method
+    // model.addFunction(function_node);
 }
 
 } // namespace Core
