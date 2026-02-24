b994160657c5aec39d33668f5ad292b3b08cce37
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-24 19:16:32 +0100

    Type struct - update structure

--- src/core/Type.hpp
+++ src/core/Type.hpp
@@ -16,17 +16,31 @@ struct Type
 {
     NodeId id;
     TypeKind kind;
-    std::string name;
-    int size; // in bytes
-    // TODO: scope?
-    // TODO: source_location?
-    // TODO: array_size?
-    // TODO: ptr_type (pointer, lvalue ref, rvalue ref)?
+    std::string name; // "int", "struct Foo", ...
 
-    bool is_const;
-    bool is_unsigned;
-    // TODO: is_volatile?
+    // size and memory layout
+    int size_bits;  // for bitfields
+    int size_bytes; // sizeof
+    int alignment;  // in bytes
 
+    // qualifiers and attributes
+    bool is_const = false;
+    bool is_volatile = false;
+    bool is_restrict = false;
+    bool is_atomic = false;
+
+    // type specifics
+    bool is_unsigned = false;
+    bool is_struct = false;
+    bool is_union = false;
+
+    // number of elements for arrays (0 for VLA or flexible array member)
+    int array_element_count = 0;
+
+    // structure
+    // - for ptr/array: [0] is the pointee/element type
+    // - for struct/union: list of field declarations (variables)
+    // - for function: [0] return type, rest are parameter types
     std::vector<NodeId> nested_type_ids;
 };
 
