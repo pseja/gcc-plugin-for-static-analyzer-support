786075f2bbbbd032b785cc2a04991c68b0282a31
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-24 19:15:23 +0100

    StorageDuration enum class

---
+++ src/core/StorageDuration.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+
+namespace CodeListener
+{
+
+namespace Core
+{
+
+enum class StorageDuration
+{
+    AUTO,
+    STATIC,
+    EXTERN,
+    REGISTER,
+    THREAD_LOCAL,
+};
+
+inline std::string toString(StorageDuration storage_duration)
+{
+    switch (storage_duration)
+    {
+    case StorageDuration::AUTO:
+        return "AUTO";
+    case StorageDuration::STATIC:
+        return "STATIC";
+    case StorageDuration::EXTERN:
+        return "EXTERN";
+    case StorageDuration::REGISTER:
+        return "REGISTER";
+    case StorageDuration::THREAD_LOCAL:
+        return "THREAD_LOCAL";
+    default:
+        return "UNKNOWN";
+    }
+}
+
+} // namespace Core
+
+} // namespace CodeListener
