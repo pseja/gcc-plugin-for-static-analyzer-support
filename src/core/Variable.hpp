85c7f7873cd6a71da77733771042eb75524091ea
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-24 19:17:14 +0100

    Variable struct - update structure

--- src/core/Variable.hpp
+++ src/core/Variable.hpp
@@ -2,9 +2,11 @@
 
 #include <string>
 
+#include "Linkage.hpp"
 #include "NodeId.hpp"
 #include "Scope.hpp"
 #include "SourceLocation.hpp"
+#include "StorageDuration.hpp"
 
 namespace CodeListener
 {
@@ -17,8 +19,18 @@ struct Variable
     NodeId id;
     std::string name;
     NodeId type_id;
+
     SourceLocation source_location;
     Scope scope;
+
+    // C storage semantics
+    StorageDuration storage_duration = StorageDuration::AUTO;
+    Linkage linkage = Linkage::NONE;
+
+    // bitfields in structs
+    bool is_bitfield = false;
+    int bitfield_size = 0;   // in bits
+    int bitfield_offset = 0; // in bits from the start of the storage unit
 };
 
 } // namespace Core
