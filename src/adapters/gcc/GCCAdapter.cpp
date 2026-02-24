7721967961b91a728b9366445b43b27d387aa5a5
Lukas Pseja <luk4sps3j4@gmail.com>
2026-02-24 20:12:18 +0100

    GCCAdapter - improve processFunction to handle GIMPLE_GOTO

--- src/adapters/gcc/GCCAdapter.cpp
+++ src/adapters/gcc/GCCAdapter.cpp
@@ -796,6 +796,13 @@ void GCCAdapter::processFunction(function *fun)
                 instruction_node.is_terminator = true;
                 break;
             }
+            case GIMPLE_GOTO: {
+                instruction_node.kind = InstructionKind::GOTO;
+                ggoto *goto_stmt = as_a<ggoto *>(stmt);
+                instruction_node.operands.push_back(parseOperand(gimple_goto_dest(goto_stmt)));
+                instruction_node.is_terminator = true;
+                break;
+            }
             block_node.instruction_ids.push_back(instruction_node.id);
         }
 
