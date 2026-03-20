ab4fc4d82e9979eed762b41c48ea9c6a491847da
Lukas Pseja <luk4sps3j4@gmail.com>
2026-03-20 19:16:36 +0100

    refactor Operand and Accessor fat structs with std::variant and to be inside Operand.hpp (for now) to avoid circular dependency issues

--- src/core/Operand.hpp
+++ src/core/Operand.hpp
@@ -1,13 +1,76 @@
 #pragma once
 
 #include <variant>
+#include <vector>
 
-#include "ConstantOperand.hpp"
-#include "VariableOperand.hpp"
+#include "AccessorKind.hpp"
+#include "TypeId.hpp"
+#include "VariableId.hpp"
 
 namespace CodeListener::Core
 {
 
-using Operand = std::variant<VariableOperand, ConstantOperand>;
+struct Accessor;
+
+struct ConstantOperand
+{
+    TypeId type_id;
+    std::string value;
+};
+
+struct VariableOperand
+{
+    VariableId id;
+    std::vector<Accessor> access_path;
+};
+
+using Operand = std::variant<ConstantOperand, VariableOperand>;
+
+struct DerefAccessor
+{
+    // *ptr
+};
+
+struct ArrayAccessor
+{
+    // ptr[index] or arr[index]
+    Operand index;
+};
+
+struct FieldAccessor
+{
+    // obj.field or ptr->field
+    VariableId field_id;
+};
+
+struct AddressOfAccessor
+{
+    // &expr
+    TypeId target_type_id;
+};
+
+struct OffsetAccessor
+{
+    // (char*)ptr + offset (pointer arithmetic)
+    Operand offset;
+};
+
+struct BitSliceAccessor
+{
+    // expr[start:end] (bit field extraction)
+    unsigned bit_start;
+    unsigned bit_size;
+};
+
+using AccessorData =
+    std::variant<DerefAccessor, ArrayAccessor, FieldAccessor, AddressOfAccessor, OffsetAccessor, BitSliceAccessor>;
+
+struct Accessor
+{
+    // FIXME: not needed anymore thanks to the AccessorData variant, but keeping it for easier querying and debugging
+    // for now
+    AccessorKind kind;
+    AccessorData data;
+};
 
 } // namespace CodeListener::Core
